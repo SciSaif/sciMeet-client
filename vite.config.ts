107aa217c5400c0e8009467ba23b02bf0564553a
saif <saiflll284@gmail.com>
2023-04-30 23:57:48 +0530

    added vercel.json

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+//ignore file web.config
+
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
