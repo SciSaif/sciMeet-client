dc111f1bc800653849d37c875a13d6e589a6c45b
saif <saiflll284@gmail.com>
2023-09-30 19:44:21 +0530

    profile page

--- src/pages/dashboard/sidebar/components/ProfileBar.tsx
+++ src/pages/dashboard/sidebar/components/ProfileBar.tsx
@@ -24,37 +24,43 @@ const ProfileBar = () => {
                 onClick={() => dispatch(toggleProfile())}
                 className="flex flex-row justify-between items-center px-2 bg-[#161f2c] py-2 gap-x-2 h-fit cursor-pointer   w-full "
             >
-                <div className="flex flex-row items-center gap-x-2">
-                    <div className="flex rounded-full relative group-hover:rotate-6">
-                        <img
-                            className="h-10 w-10 rounded-full"
-                            src={
-                                user?.avatar
-                                    ? user.avatar
-                                    : "avatars/pikachu.png"
-                            }
-                            alt="dp"
-                        />
-                        <div className="absolute h-3 w-3 rounded-full  bg-primary bottom-0 right-0 ">
-                            <div className=" absolute rounded-full bottom-1/2 right-1/2 translate-x-1/2  translate-y-1/2 bg-green-500 h-[7px] w-[7px] "></div>
-                        </div>
-                    </div>
-                    <div className="text-white/80">
-                        <div>{user?.username}</div>
-                        <div className="text-xs text-textGray">Online</div>
-                    </div>
-                </div>
-                {!isProfileOpen && invitations?.length !== 0 && (
-                    <div className="pr-5">
-                        <div className="rounded-full min-w-[24px] text-center text-xs text-white bg-primary-700 p-1">
-                            {invitations.length}
-                        </div>
+                {isProfileOpen && (
+                    <div className="text-text h-10 items-center justify-center text-center w-full flex  ">
+                        <div>Close profile</div>
                     </div>
                 )}
-                {isProfileOpen && (
-                    <div className="text-text mr-2 hover:text-text1">
-                        <XMarkIcon width={20} height={20} />
-                    </div>
+                {!isProfileOpen && (
+                    <>
+                        <div className="flex flex-row items-center gap-x-2">
+                            <div className="flex rounded-full relative group-hover:rotate-6">
+                                <img
+                                    className="h-10 w-10 rounded-full"
+                                    src={
+                                        user?.avatar
+                                            ? user.avatar
+                                            : "avatars/pikachu.png"
+                                    }
+                                    alt="dp"
+                                />
+                                <div className="absolute h-3 w-3 rounded-full  bg-primary bottom-0 right-0 ">
+                                    <div className=" absolute rounded-full bottom-1/2 right-1/2 translate-x-1/2  translate-y-1/2 bg-green-500 h-[7px] w-[7px] "></div>
+                                </div>
+                            </div>
+                            <div className="text-white/80">
+                                <div>{user?.username}</div>
+                                <div className="text-xs text-textGray">
+                                    Online
+                                </div>
+                            </div>
+                        </div>
+                        {!isProfileOpen && invitations?.length !== 0 && (
+                            <div className="pr-5">
+                                <div className="rounded-full min-w-[24px] text-center text-xs text-white bg-primary-700 p-1">
+                                    {invitations.length}
+                                </div>
+                            </div>
+                        )}
+                    </>
                 )}
             </div>
             {isProfileOpen && (
